e4a3e85b3d98563edd7131aaad3311f1359edaf1
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-01-25 16:23:48 +0500

    AboutSection html completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+// import pages
+import AboutUs from "./pages/AbuotUs";
 
 function App() {
   return (
     <div className="App">
- <h1>Hello</h1>
+      <h1>E-Learning</h1>
+      <AboutUs />
     </div>
   );
 }

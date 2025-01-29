63b4d0cfe6908e8dfb253c10636b81d0bc6185ca
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-01-29 22:28:20 +0500

    Nav Completed static styled

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import AboutUs from "./pages/AbuotUs";
 // import GlobalStyle
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
       <AboutUs />
 
